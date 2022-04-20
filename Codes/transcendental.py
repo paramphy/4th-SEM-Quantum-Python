58eb27f282025721268c9bb47d97daa3f711addb
Paramesh Chandra <51922459+paramphy@users.noreply.github.com>
2022-04-20 20:25:34 +0530

    .

--- Codes/transcendental.py
+++ Codes/transcendental.py
@@ -4,17 +4,20 @@ from scipy.optimize import newton
 
 
 def leftfunc(x):
-    return(x*np.tan(x))
+    return x * np.tan(x)
+
 
 def rightfunc(x):
-    return(np.sqrt(50-x**2))
+    return np.sqrt(50 - x ** 2)
+
 
 def your_funcs(x):
 
-    f = leftfunc(x)-rightfunc(x)
+    f = leftfunc(x) - rightfunc(x)
     return f
 
-x = np.linspace(0,10,1000)
+
+x = np.linspace(0, 10, 1000)
 plt.scatter(x, leftfunc(x))
 plt.plot(x, rightfunc(x))
 plt.axis([0, 10, 0, 10])
@@ -23,4 +26,3 @@ plt.show()
 initial_guess = float(input("Input the initial guess: "))
 sol2 = newton(your_funcs, initial_guess)
 print(sol2)
-
